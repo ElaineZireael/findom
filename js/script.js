615d34a11f527607b0ccd23cd5269dc964c856c7
Alina <scallary@mail.ru>
2017-10-16 12:17:07 +1000

    main page changes

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$(".search-block").click(function(e) {
+		e.preventDefault();
+		if ($(".search-collapse").css("display") == "none")
+			$(".search-collapse").slideDown(350);
+		else
+			$(".search-collapse").slideUp(350);
+	});
+
+	$(window).resize(function() {
+		if ($(this).width() >991)
+			$(".search-collapse").css("display", "none");
+	});
+
+});
