c9061c11ee9202f796fc5449ba2ff003e5581bdd
Alina <scallary@mail.ru>
2017-10-31 10:27:00 +1000

    contacts added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+	var news_detail_pic_width = $(".news-detail .detail-picture").width();
+	$(".news-detail .detail-picture").css("height", news_detail_pic_width / 1.6285);
+
 	$(".search-block").click(function(e) {
 		e.preventDefault();
 		if ($(".search-collapse").css("display") == "none")
@@ -8,12 +11,25 @@ $(document).ready(function() {
 			$(".search-collapse").slideUp(350);
 	});
 
+	$(".slide-contact").click(function(){
+		var neighbour_children = $(this).next().find(".contact");
+		if (neighbour_children.css("display") == "block") {
+			neighbour_children.slideUp();
+		} else
+			neighbour_children.slideDown();
+	});
+
 	$(window).resize(function() {
 		if ($(this).width() > 991)
 			$(".search-collapse").css("display", "none");
 
-			var news_detail_pic_width = $(".news-detail .detail-picture").width();
-			$(".news-detail .detail-picture").css("height", news_detail_pic_width / 1.6285);
+		var news_detail_pic_width = $(".news-detail .detail-picture").width();
+		$(".news-detail .detail-picture").css("height", news_detail_pic_width / 1.6285);
+
+		if ($(this).width() > 767)
+			$(".contact").css("display", "block");
+		else
+			$(".contact").css("display", "none");
 
 	});
 
